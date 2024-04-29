8ad3fdcf58aa044dff03dfd3ab17b73a5530a2c8
Ashish Khare <akstyles211@gmail.com>
2024-04-29 13:28:44 +0530

    done: 350

---
+++ Days/348.py
@@ -0,0 +1,10 @@
+def uniqueSubstr(string):
+	pass
+
+def main():
+	assert uniqueSubstr("eceba") == 3
+	assert uniqueSubstr("ccaabbb") == 5
+	assert uniqueSubstr("abcabcabc") == 2
+
+if __name__ == '__main__':
+	main()
---
+++ Days/350.py
@@ -0,0 +1,28 @@
+def getDMAP():
+	dmap = {}
+	rows = [['wertyuiop[', 'qwertyuiop'], ['sdfghjkl;', 'asdfghjkl'], ['xcvbnm,', 'zxcvbnm']]
+
+	for row in rows:
+		n = len(row[0])
+		for index in range(n):
+			dmap[row[0][index]] = row[1][index]
+
+	return dmap
+
+def translateRightShift(string):
+	dmap = getDMAP()
+	res = ''
+	for char in string:
+		if char in dmap:
+			res += dmap[char]
+		else:
+			res += char
+	return res
+	
+
+def main():
+	assert translateRightShift(";p; epeor") == "lol wowie"
+	assert translateRightShift("ejp s, o") == "who am i"
+
+if __name__ == '__main__':
+	main()
